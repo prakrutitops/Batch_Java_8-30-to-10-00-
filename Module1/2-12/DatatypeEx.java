a7b24ed4fcbfcaf9f75b250b16ed63684fcef4fc
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2022-12-02 09:32:02 +0530

    Add files via upload

---
+++ Module1/2-12/DatatypeEx.java
@@ -0,0 +1,25 @@
+
+public class DatatypeEx 
+{
+	public static void main(String[] args) {
+		
+		
+		
+		int number = 20;
+		
+		int number2 = 30;
+		
+		
+		int add = number2 +number;
+		int sub = number2 -number;
+		int mul =number * number2;
+		float div = number2/number;
+		
+		System.out.println(add);
+		System.out.println(sub);
+		System.out.println(mul);
+		System.out.println(div);
+		
+		
+	}
+}
---
+++ Module1/2-12/DetailPrint.java
@@ -0,0 +1,14 @@
+
+public class DetailPrint 
+{
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Name: Tejas");
+		System.out.println("Surname: Akbari");
+		System.out.println("Email: Tejasakbari@gmail.com");
+		System.out.println("Mobile :9999999999");
+		
+	}
+}
+
