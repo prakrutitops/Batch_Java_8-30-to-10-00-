06ab59d69f5c419c5d08facd6eec8f086cb3f084
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2022-12-23 09:35:08 +0530

    Add files via upload

---
+++ Module1/23-12-GUI/23-12/InputDialogEx.java
@@ -0,0 +1,29 @@
+package com.a2312;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class InputDialogEx 
+{
+	JFrame frame;
+	
+	public InputDialogEx() 
+	{
+		// TODO Auto-generated constructor stub
+		frame =new JFrame();
+		
+		frame.setSize(300,300);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		
+		String name = JOptionPane.showInputDialog(frame, "Enter Your Name");
+		System.out.println("Welcome "+name);
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+		new InputDialogEx();
+	}
+}
+
---
+++ Module1/23-12-GUI/23-12/MessageDialogEx.java
@@ -0,0 +1,28 @@
+package com.a2312;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class MessageDialogEx 
+{
+	JFrame frame;
+	
+	public MessageDialogEx() 
+	{
+		// TODO Auto-generated constructor stub
+		frame =new JFrame();
+		
+		frame.setSize(300,300);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		
+		JOptionPane.showMessageDialog(frame, "Hello It's a Warning Message!!");
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+		new MessageDialogEx();
+	}
+}
