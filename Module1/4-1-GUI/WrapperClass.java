132d44365cbf5cca1cb0d5b15ee99a5098b849e1
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-01-04 10:22:47 +0530

    Add files via upload

---
+++ Module1/4-1-GUI/WrapperClass.java
@@ -0,0 +1,19 @@
+package com.a21;
+
+public class WrapperClass 
+{
+	public static void main(String[] args) {
+		
+		
+		String a = "5";
+		String b = "6";
+		
+		int c= Integer.parseInt(a)+Integer.parseInt(b);
+		
+		System.out.println(c);
+		
+	}
+}
+
+//int - data type
+//Integer - Wrapper Class
